3edd26893b50960c5c59b806b3cc5eb2766a36fb
JoaoPedroLouvre <joao.marques@louvreconsulting.com.br>
2024-12-17 15:50:13 -0300

    tentativa de criar dotenv

--- nodeJS/enviarDados/config.js
+++ nodeJS/enviarDados/config.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
+require('dotenv').config()
 
 const banco = new Pool({
     user: 'myadmin',
     host: 'localhost',
     database: 'mypoint',
     password: 'Louvre@2024',
-    port: 5432
+    port: '5432'
 })
 
 module.exports = banco
